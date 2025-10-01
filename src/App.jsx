f5a9d5d9c19529eadc99ecb3b3832b759620d291
br4vocode <david.alfonso.hdz.05@gmail.com>
2025-10-01 17:05:28 -0400

    fix: use relative paths for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
       <div className="flex items-center justify-center min-h-screen bg-slate-100 p-6">
-        <QrCard qr="/images/image-qr-code.png" />
+        <QrCard qr="./images/image-qr-code.png" />
       </div>
     </>
   );
